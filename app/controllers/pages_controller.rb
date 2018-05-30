de472af1e5c250a9b5a0fc1cc59eac74a5e3eaa8
ram <ramarjunkumar@gmail.com>
2018-05-30 22:42:21 +0530

    Add new pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def frames
+  end
+
+  def keyholder
+  end
+
+  # def contact
+  # end
+
+  # def mail
+  #   UserMailer.contact(params).deliver_now
+  # end
+
+
+end
+
+
+
