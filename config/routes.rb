cd346feb56ff011237f6431b7f6f101500df03af
ramarjunan <ramarjunkumar@gmail.com>
2018-07-25 18:32:09 +0530

    Product changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get '/frames', to: 'pages#frames'
-   get '/keyholder', to: 'pages#keyholder'
+   get '/products', to: 'pages#product'
+   # get '/keyholder', to: 'pages#keyholder'
    get '/about', to: 'pages#about'
-   get '/flower', to: 'pages#flower'
+   # get '/flower', to: 'pages#flower'
     # get '/contact', to: 'pages#contact'
     # post '/mail', to: 'pages#mail'
 end
